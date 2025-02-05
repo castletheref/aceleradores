03e1ab6de9f0b230c2ea2197018cd8f57b7ab9d5
castletheref <110067261+castletheref@users.noreply.github.com>
2025-02-05 13:13:26 -0600

    Update aceleradores.py

--- aceleradores.py
+++ aceleradores.py
@@ -13,26 +13,27 @@ def load_data():
     return df
 
 df = load_data()
+#Zona	Nombre del gestor	Bucket Inicial	Cuentas Asignadas	Cuentas Contenidas	Flujo Domiciliacion	Porcentaje de contencion	Arancel	Monto Acelerador	Distrito
 
-st.title("📊 Dashboard de Aceleradores")
+st.title("📊 Dashboard Aceleradores Campo")
 
 # Filtros
-zona_seleccionada = st.selectbox("Selecciona una Zona", df["zona_campo"].unique())
-gestor_seleccionado = st.selectbox("Selecciona un Gestor", df[df["zona_campo"] == zona_seleccionada]["name"].unique())
+zona_seleccionada = st.selectbox("Selecciona una Zona", df["Zona"].unique())
+gestor_seleccionado = st.selectbox("Selecciona un Gestor", df[df["Zona"] == zona_seleccionada]["Nombre del gestor"].unique())
 
-df_filtrado = df[(df["zona_campo"] == zona_seleccionada) & (df["name"] == gestor_seleccionado)]
+df_filtrado = df[(df["Zona"] == zona_seleccionada) & (df["Nombre del Gestor"] == gestor_seleccionado)]
 
 # KPIs
 col1, col2, col3, col4 = st.columns(4)
-col1.metric("Cuentas Asignadas", df_filtrado["cuentas_asig"].sum())
-col2.metric("Cuentas Contenidas", df_filtrado["cuentas_cont"].sum())
-col3.metric("Flujo DOMI", f"${df_filtrado['Flujo_DOMI'].sum():,.2f}")
+col1.metric("Cuentas Asignadas", df_filtrado["Cuentas Asignadas"].sum())
+col2.metric("Cuentas Contenidas", df_filtrado["Cuentas Contenidas"].sum())
+col3.metric("Flujo domiciliacion", f"${df_filtrado['Flujo Domiciliacion'].sum():,.2f}")
 # Calcular la suma de la columna 'acelerador' para la zona seleccionada
-suma_acelerador = df_filtrado["acelerador"].sum()
+suma_acelerador = df_filtrado["Monto Acelerador"].sum()
 
 # Mostrar el insight adicional
 #col4 = st.columns(1)[0]
-col4.metric("Suma Aceleradores", f"{suma_acelerador:,.2f}")
+col4.metric("Monto Aceleradores", f"{suma_acelerador:,.2f}")
 
 
 # Tabla de datos
